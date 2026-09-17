b3a669b14d19d483b6d73ae798a4296ca2c84211
Claire Barnes <claire@puzzledpigeon.com>
2026-09-17 15:47:28 +1000

    feat: build JS shim file

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+set -e
+
 source ~/.ghc-wasm/env
+
+# Build the wasm file
 wasm32-wasi-cabal --with-compiler=wasm32-wasi-ghc --with-ghc-pkg=wasm32-wasi-ghc-pkg build
+
+# Get its location
+out="$(wasm32-wasi-cabal list-bin trails)"
+
+# Build the shim file
+$(wasm32-wasi-ghc --print-libdir)/post-link.mjs -i "$out" -o trails.js
