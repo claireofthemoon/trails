32200162ce9c5b96fc3d9452113eec36a412f40f
Claire Barnes <claire@puzzledpigeon.com>
2026-09-17 14:18:41 +1000

    feat: build WASI reactor module

---
+++ app/Trails.hs
@@ -0,0 +1,7 @@
+module Trails (fib) where
+
+foreign export ccall fib :: Int -> Int
+
+fib :: Int -> Int
+fib 0 = 1
+fib n = n * fib (n - 1)
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source ~/.ghc-wasm/env
-cabal --with-compiler=wasm32-wasi-ghc --with-ghc-pkg=wasm32-wasi-ghc-pkg build
+wasm32-wasi-cabal --with-compiler=wasm32-wasi-ghc --with-ghc-pkg=wasm32-wasi-ghc-pkg build
